ac4c0710e2fce01a9f65bb52b12391758cd10b19
Hannah Howard <hannah@hannahhoward.net>
2013-07-18 14:24:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbTunes::Application.config.secret_token = '5061bc2d406789b719879165e2a5625e3385a101a9d05516af06d9f39cfcaa4b791604cccc5a5cdcfdf9b55d403b7706fd202eb95ed9da62905ec394fbe92e14'
