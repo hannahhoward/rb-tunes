1ee0783673ed8ae9703629b8d5494960dd98b9da
Hannah Howard <hannah@hannahhoward.net>
2013-07-18 14:48:17 -0700

    Initial welcome and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def body_classes
+    body_classes = [] || @body_classes
+    body_classes << params[:controller] if params[:controller]
+    body_classes << params[:action] if params[:action]
+    body_classes.join(" ")
+  end
+
+  def add_body_class(klass)
+    @body_classes ||= []
+    @body_classes << klass
+  end
+
 end
