612e1d92331f8bee41ed8364811187471e589949
Hannah Howard <hannah@hannahhoward.net>
2013-07-18 17:49:48 -0700

    Remove all models

--- db/migrate/20130719002031_create_playlists_songs.rb
+++
@@ -1,8 +0,0 @@
-class CreatePlaylistsSongs < ActiveRecord::Migration
-  def change
-    create_table :playlists_songs do |t|
-      t.integer :playlist_id
-      t.integer :song_id
-    end
-  end
-end
